442043b1a1d82ca9489d27bc5acb14a51e079f7e
dmarcela <dmarcela@undanet.com>
2019-04-03 14:26:55 +0200

    save 3/4

---
+++ section 15/Section Code (Start)/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-http-4f77a.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => { return {message: response.body} }
+  });
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
